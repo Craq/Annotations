c147ca271c879142835f816fc81d8e95155f51d8
Craq <protion86@gmail.com>
2019-03-03 15:21:32 +0200

    added os

--- src/main/java/Galaxy.java
+++ src/main/java/Galaxy.java
@@ -11,9 +11,16 @@ public class Galaxy {
     @Autowired
     @Qualifier("snapdragon")
     CPU cpu;
+    String os;
     String name;
     public Galaxy(String name){
         this.name = name;
+    }
+    public void setOs(String os){
+        this.os = os;
+    }
+    public String getOs(){
+        return this.os;
     }
     public void getPhoneInfo(){
         System.out.println("Name: "+name);
